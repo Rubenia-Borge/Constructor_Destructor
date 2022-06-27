c221a7a5c6e3928af9a4ba789ded69f6a6706aba
Rubenia Borge <rubeniaolaska@gmail.com>
2022-06-27 00:17:01 -0500

    Update problem_without_copy_constructor.cpp

--- lecture_examples/problem_without_copy_constructor.cpp
+++ lecture_examples/problem_without_copy_constructor.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+using namespace std;
+
+class MyClass {
+  public:
+     MyClass() {
+        cout << "Constructor called." << endl;
+        dataObject = new int; // Allocate data object
+        *dataObject = 0;
+     }
+     ~MyClass() {
+        cout << "Destructor called." << endl;
+        delete dataObject;
+     }
+     void SetDataObject(const int i) { *dataObject = i; }
+     int GetDataObject() const { return *dataObject; }
+
+  private:
+     int* dataObject;
+};
+
+void SomeFunction(MyClass localObject) {
+  // Do something with localObject
+}
+
+int main() {
+  MyClass tempClassObject; // Create object of type MyClass
+ 
+  // Set and print data member value
+  tempClassObject.SetDataObject(9);
+  cout << "Before: " << tempClassObject.GetDataObject() << endl;
+ 
+  // Calls SomeFunction(), tempClassObject is passed by value
+  SomeFunction(tempClassObject);          
+ 
+  cout << "After: " << tempClassObject.GetDataObject() << endl;
+
+  return 0;
+}
